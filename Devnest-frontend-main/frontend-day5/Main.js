549a23881d991a41bd2abd3c81d1635038e5ccc3
Ross Nelson <rdnelson2010@gmail.com>
2021-06-11 12:55:41 -0400

    day 5 done!

---
+++ Devnest-frontend-main/frontend-day5/Main.js
@@ -0,0 +1,5 @@
+function calc() {
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var op
+}
---
+++ Devnest-frontend-main/index.js
@@ -0,0 +1,23 @@
+function clone(arr){
+    var brr = arr;
+    console.log("The cloned array is:" ,brr)
+    return 0;
+}
+
+function firstn(arr, n){
+    var len arr.length;
+    for(let i = 0;i<n;i++) {
+        console.log(arr[i]);
+    }
+    console.log("the length of the array is:",len);
+}
+
+firstn([1,2,3,4,5,6], 3);
+
+function joinstr(arr) {
+    lstr=arr.join("")
+    console.log(str)
+    }
+
+    joinstr(["Hello", "world", "this" , "javascript"]);
+}
